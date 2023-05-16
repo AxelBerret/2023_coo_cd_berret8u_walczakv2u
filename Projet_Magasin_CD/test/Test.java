f719d0a5e19ca1a917d515d8629b3fa03f148d09
Axel Berret <berret8u@etu.univ-lorraine.fr>
2023-05-16 12:00:05 +0200

    ajout des test unitaires

--- Projet_Magasin_CD/test/Test.java
+++ Projet_Magasin_CD/test/Test.java
@@ -1,5 +1,24 @@
-import java.io.IOException;
+import org.testng.annotations.Test;
 
-public class Test {
-    
+import java.io.IOException;
+public class TestChargeurMagasin {
+
+    @Test
+    public void testChargerMagasinOK() throws IOException {
+        ChargeurMagasin chargeurMagasin = new ChargeurMagasin("../magasinCD donnees/musicbrain:Simple");
+
+        Magasin m chargeurMagasin.chargerMagasin();
+
+        int nombre m.getNombreCds();
+
+        assertEquals("magasin devrait avoir 12 Cds", 12, nombre):
+
+    }
+
+    @Test(expected=I0Exception.class)
+
+    public void testChargerMagasinInconnu() throws IOException {
+        Chargeur Magasin chargeurMagasin = new Chargeur Magasin("../magasinCD donnees/X");
+        chargeurMagasin.chargerMagasin();
+    }
 }
