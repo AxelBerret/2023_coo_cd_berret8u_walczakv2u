29c294f64b7626623b9746ab36ce5efe0ff686c7
Axel Berret <berret8u@etu.univ-lorraine.fr>
2023-05-19 08:12:26 +0200

    mise a jour des tests 1

--- Projet_Magasin_CD/test/Test.java
+++ Projet_Magasin_CD/test/Test.java
@@ -1,24 +1,19 @@
-import org.testng.annotations.Test;
-
+import
 import java.io.IOException;
-public class TestChargeurMagasin {
+
+public class Test {
 
     @Test
     public void testChargerMagasinOK() throws IOException {
         ChargeurMagasin chargeurMagasin = new ChargeurMagasin("../magasinCD donnees/musicbrain:Simple");
-
-        Magasin m chargeurMagasin.chargerMagasin();
-
-        int nombre m.getNombreCds();
-
+        Magasin m = chargeurMagasin.chargerMagasin();
+        int nombre = m.getNombreCds();
         assertEquals("magasin devrait avoir 12 Cds", 12, nombre):
-
     }
 
     @Test(expected=I0Exception.class)
-
     public void testChargerMagasinInconnu() throws IOException {
-        Chargeur Magasin chargeurMagasin = new Chargeur Magasin("../magasinCD donnees/X");
+        Chargeur chargeurMagasin = new Chargeur("../magasinCD donnees/X");
         chargeurMagasin.chargerMagasin();
     }
 }
