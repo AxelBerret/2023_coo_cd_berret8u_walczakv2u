1ac791beb8987d47e78247abb9efdfeda4e2882e
DIMI_TRIE <dimitriwzk@gmail.com>
2023-05-19 08:30:02 +0200

    Update Test

--- Projet_Magasin_CD/test/TestChargeurMagasin.java
+++ Projet_Magasin_CD/test/TestChargeurMagasin.java
@@ -1,19 +1,22 @@
-import
+import org.junit.Test;
+
 import java.io.IOException;
 
-public class Test {
+import static org.junit.Assert.assertEquals;
+
+public class TestChargeurMagasin {
 
     @Test
     public void testChargerMagasinOK() throws IOException {
         ChargeurMagasin chargeurMagasin = new ChargeurMagasin("../magasinCD donnees/musicbrain:Simple");
         Magasin m = chargeurMagasin.chargerMagasin();
         int nombre = m.getNombreCds();
-        assertEquals("magasin devrait avoir 12 Cds", 12, nombre):
+        assertEquals("magasin devrait avoir 12 Cds", 12, nombre);
     }
 
-    @Test(expected=I0Exception.class)
+    @Test(expected=IOException.class)
     public void testChargerMagasinInconnu() throws IOException {
-        Chargeur chargeurMagasin = new Chargeur("../magasinCD donnees/X");
+        ChargeurMagasin chargeurMagasin = new ChargeurMagasin("../magasinCD donnees/X");
         chargeurMagasin.chargerMagasin();
     }
 }
