a0c5e296aefbf59d1d0d95f589187a70f578f918
DIMI_TRIE <dimitriwzk@gmail.com>
2023-05-23 10:59:59 +0200

    Ajout JavaDoc pour comparerAlbum

--- Projet_Magasin_CD/src/ComparateurAlbum.java
+++ Projet_Magasin_CD/src/ComparateurAlbum.java
@@ -1,4 +1,11 @@
 public class ComparateurAlbum implements ComparateurCd{
+
+    /**
+     *
+     * @param cd1
+     * @param cd2
+     * @return
+     */
     @Override
     public boolean etreAvant(CD cd1, CD cd2) {
         return cd1.getNomCD().compareTo(cd2.getNomCD())<0;
